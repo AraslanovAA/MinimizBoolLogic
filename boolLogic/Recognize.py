33430fe0022ad325f7bf63d64705cb29b8df169e
AraslanovAA <araslanov.anton.p@gmail.com>
2019-02-24 00:48:26 +0300

    Функция vocabulary на вход получает функцию(string), проверяет правильность написания и преобразует в логические выражения распознаваемые питоном (not, or, and) используя библиотеки truths.py и prettytable.py реализует вывод таблицы истинности

--- boolLogic/Recognize.py
+++ boolLogic/Recognize.py
@@ -1,4 +1,5 @@
-
+#todo: прежде чем отдавать параметры на создание таблицы истинности, создать новый список без 0 и 1 либо обработать 0 и 1 уже в дургом классе
+from truths import Truths
 global OpNames#список с именами параметров
 OpNames = []
 def recognizeOperands(letter,Func,Sygnal,N=4):
@@ -48,6 +49,7 @@ pirs	:	'/'
 imp	:	'#' наверно чуть логичнее было бы импользовать > ?'''
 #короткие имена разрешено писать слитно, имспользуя длинные имена необходимо ставить пробелы
 def vocabulary(Func):
+    global ShortNames
     ShortNames ={
         'not': '-',
         'or': '+',
@@ -59,24 +61,22 @@ def vocabulary(Func):
     }
     LFunc =[]
     LFunc = list(Func)
-    for i in LFunc:
-        if i == '(':
-            i = ' ( '
-        if i == ')':
-            i =' ) '
+    for i in range(LFunc.__len__()):
+        if LFunc[i] == '(':
+            LFunc[i] = ' ( '
+        if LFunc[i] == ')':
+            LFunc[i] = ' ) '
     Func = ""  # собираем строку обратно
     for Item in LFunc:
         Func = Func + Item
     LFunc.clear()
     LFunc = Func.split(' ')
-    print(LFunc)
     while '' in LFunc:
         LFunc.pop(LFunc.index(''))
     for ItemIndex in range(len(LFunc)):#заменяем длинные имена на короткие
         if LFunc[ItemIndex] in ShortNames:
             LFunc[ItemIndex] = ShortNames[LFunc[ItemIndex]]
 
-    print(LFunc)
     Func =""#собираем строку обратно
     for Item in LFunc:
         Func = Func + Item
@@ -86,13 +86,13 @@ def vocabulary(Func):
     X='X0'
     openskobka = 0
     recognized = False
-    # пиздец сука говнокод нахуй switch case бы блять
+    global Sygnal
     Sygnal = {
         '-': 1,
         '(': 2,
         ')': 3,
-        '+': 4,
-        '*': 5,
+        '*': 4,
+        '+': 5,
         '%': 6,  # xor
         '|': 7,
         '/': 8,
@@ -143,7 +143,7 @@ def vocabulary(Func):
                 if (Sygnal[Func[letter]] == 3):
                     X = 'X5'
                 else:
-                    if ((Sygnal[Func[letter]] >=4) and (Sygnal[Func[letter]] <=9)):
+                    if (Sygnal[Func[letter]] >=4):
                         X='X4'
                     else:
                         print('некорректное выражение, символ: ' + str(letter + 1))# todo:передать сообщение на экран
@@ -172,7 +172,7 @@ def vocabulary(Func):
                 if (Sygnal[Func[letter]] == 3):
                     X= 'X5'
                 else:
-                    if ((Sygnal[Func[letter]] >=4) and(Sygnal[Func[letter]] <=9)):
+                    if (Sygnal[Func[letter]] >=4):
                         X='X4'
                     else:
                         print('некорректное выражение, символ: ' + str(letter + 1))# todo:передать сообщение на экран
@@ -193,13 +193,149 @@ def vocabulary(Func):
         letter += 1
 
     if (openskobka < 0):
-        print("Обработка ошибки: перевес закрывающих скобок символ: " + str(letter + 1))
+        print("Обработка ошибки: перевес закрывающих скобок символ: " + str(letter + 1))# todo:передать сообщение на экран
         return -1
     if openskobka != 0:
-        print("Обработка ошибки: перевес открывающих скобок в выражении")
+        print("Обработка ошибки: перевес открывающих скобок в выражении")# todo:передать сообщение на экран
         return -1
     print("Твоя функция записана верно")
-    print(Func)
-    print(OpNames)
+    #print(Func)
+    #print(OpNames)
+    transform(Func)
+    print(Truths(OpNames,[transform(Func)]))
+
+def SearcArgs(Func, i,Sygnal):
+    '''функция на вход принимает строку функции и индекс логического выражения
+        на выходе индексы границ правого и левого операнда указанного логического выражения'''
+    #сначала напишем обработчик поиска левого аргумента
+    Result = []
+    k = i
+    numCloseBrackets = 0
+    flag = False
+    while flag == False:
+        if k != 0:
+            k-=1
+            if Func[k] in Sygnal:
+                if Sygnal[Func[k]] == 3:
+                    numCloseBrackets+=1
+                if Sygnal[Func[k]] == 2:
+                    if numCloseBrackets ==0:#если встретили открывающую скобку, первой то операнд уже считан
+                        Result.append(k+1)
+                        Result.append(i-1)
+                        flag=True
+                    else:
+                        numCloseBrackets-=1
+                if ((Sygnal[Func[k]]>=5) and(numCloseBrackets ==0)):#если скобки парно закрыты и встречен следущий управляющий символ, то формируем левый операнд
+                    Result.append(k+1)
+                    Result.append(i-1)
+                    flag=True
+        else:#если дошли до конца функции, то все с 0 символа до i-1 искомый операнд
+            Result.append(0)
+            Result.append(i-1)#в теории можно и без этого обойтись, но может быть загвоздка в будущем
+            flag = True
+
+    #теперь обрабатываем операнд справа
+    k = i
+    numCloseBrackets = 0
+    flag = False
+    while flag == False:
+        if k != len(Func)-1:
+            k += 1
+            if Func[k] in Sygnal:
+                # если встретили одну из скобок
+                # если встретили A4-9
+                if Sygnal[Func[k]] == 3:
+                    if numCloseBrackets ==0:#если встретили закрывающую скобку, первой то операнд уже считан
+                        Result.append(i+1)
+                        Result.append(k-1)
+                        flag=True
+                    else:
+                        numCloseBrackets += 1
+                if Sygnal[Func[k]] == 2:
+                    numCloseBrackets -= 1
+                if ((Sygnal[Func[k]] >= 5) and (#умножение разрешено очевидно
+                        numCloseBrackets == 0)):  # если скобки парно закрыты и встречен следущий управляющий символ, то формируем левый операнд
+                    Result.append(i + 1)
+                    Result.append(k - 1)
+                    flag = True
+        else:  # если дошли до конца функции, то все с i+1 до конца FUnc
+            Result.append(i+1)
+            Result.append(len(Func)-1)
+            flag = True
+    return Result
+
+def substitution(Func):
+    '''функция заменяет короткие имена на логические выражения воспринимаемы питоном'''
+    flag = False
+    while flag == False:
+        flag = True
+        for i in range(len(Func)):
+            if ((Func[i] == '+') or (Func[i] == '*') or (Func[i] == '-')):
+                flag =False
+            if (Func[i] == '+'):
+                Func = Func[:i] + ' or ' + Func[(i+1):]
+            if (Func[i] == '*'):
+                Func = Func[:i] + ' and ' + Func[(i+1):]
+            if (Func[i] == '-'):
+                Func = Func[:i] + ' not ' + Func[(i+1):]
+    return Func
+
+
+def transform(Func):
+    '''функция получает на вход проверенную функцию, преобразует все логические выражения в базис и или не, затем
+    вызывает функцию substitution  и после должна передать получившееся выражение для создания таблицы истинности'''
+    Sygnal = {
+        '-': 1,
+        '(': 2,
+        ')': 3,
+        '*': 4,
+        '+': 5,
+        '%': 6,  # xor
+        '|': 7,
+        '/': 8,
+        '#': 9  # импликация
+        }
+    argsindex =[]
+    LeftArg =""
+    RightArg=""
+    flag = False
+    moved = True
+    while flag == False:
+        flag=True
+        for i in range(len(Func)):
+            moved = True
+            if Func[i] in Sygnal:
+                if(Sygnal[Func[i]]>=6):
+                    flag=False
+                if moved == True:
+                    if(Sygnal[Func[i]] == 6):
+                        argsindex = SearcArgs(Func,i,Sygnal)
+                        LeftArg =Func[argsindex[0] : argsindex[1]+1]#интересный факт: вырезать подстроку из строки
+                        RightArg=Func[argsindex[2] : argsindex[3]+1]#левая координата указывает на индекс правая на индекс+1
+                        Func = Func[:argsindex[0]] + '(-' +LeftArg +'*'+RightArg + '+' + LeftArg +'*-' + RightArg +')'+ Func[(argsindex[3]+1):]
+                        moved = False
+                if moved == True:
+                    if (Sygnal[Func[i]] == 7):
+                        argsindex = SearcArgs(Func, i, Sygnal)
+                        LeftArg = Func[argsindex[0]: argsindex[1] + 1]
+                        RightArg = Func[argsindex[2]: argsindex[3] + 1]
+                        Func = Func[:argsindex[0]] + '(-' + LeftArg + '+-' + RightArg + ')' + Func[(argsindex[3] + 1):]
+                        moved = False
+                if moved == True:
+                    if (Sygnal[Func[i]] == 8):
+                        argsindex = SearcArgs(Func, i, Sygnal)
+                        LeftArg = Func[argsindex[0]: argsindex[1] + 1]
+                        RightArg = Func[argsindex[2]: argsindex[3] + 1]
+                        Func = Func[:argsindex[0]] + '(-' + LeftArg + '*-' + RightArg + ')' + Func[(argsindex[3] + 1):]
+                        moved = False
+                if moved == True:
+                    if (Sygnal[Func[i]] == 9):
+                        argsindex = SearcArgs(Func, i, Sygnal)
+                        LeftArg = Func[argsindex[0]: argsindex[1] + 1]
+                        RightArg = Func[argsindex[2]: argsindex[3] + 1]
+                        Func = Func[:argsindex[0]] + '(-' + LeftArg + '+' + RightArg + ')' + Func[(argsindex[3] + 1):]
+                        moved = False
+    return substitution(Func)
+
 
 vocabulary(input())
